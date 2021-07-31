c812af969982fc2f2c1bfbaa7251a280e0c39fed
Steven <stevenharris434@gmail.com>
2021-07-31 13:00:16 -0700

    Add som animation

---
+++ animate_som.py
@@ -0,0 +1,137 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import time
+
+def animate_gradient(Wnew, n, m, gradImgs):
+  for i in range(n):
+    m[:,:,i] = (Wnew[i,:]).reshape((SOMsize, SOMsize))
+  img = plt.imshow(m, animated=True)
+  gradImgs.append([img])
+  return gradImgs
+
+def animate_nodes(winningNodes, m, nodeImgs):
+  print('number of winning nodes: ' + str(len(winningNodes)))
+  winningPlot = np.zeros((m.shape))
+  count = 0
+  for (row, col) in winningNodes:
+    count += 1
+    winningPlot[row,col] = m[row,col]
+    img = plt.imshow(winningPlot, animated=True)
+    if (count % 30) == 0:
+      nodeImgs.append([img])
+  img = plt.imshow(winningPlot, animated=True)
+  nodeImgs.append([img])
+  return nodeImgs
+
+def gen_som(X, SOMsize, saveAnimation):
+  fig = plt.figure()
+  plt.axis('off')
+  imgs = []
+  (N, d) = X.shape
+  n = d
+  m = SOMsize ** 2
+  Wnew = np.random.rand(n, m) * 255
+  for i in range(m):
+    Wnew[:,i] = Wnew[:,i] / np.linalg.norm(Wnew[:,i])
+  winningNodes = set()
+  error = np.Inf
+  tolerance = 10e-8
+  eta = 0.9
+  sigma = SOMsize / 5
+  alpha = 0.99
+  iter = 0
+  a = np.zeros((SOMsize, SOMsize))
+  Wold = np.zeros((n, m))
+  p = np.zeros((SOMsize, SOMsize, 2))
+  for i in range(SOMsize):
+    for j in range(SOMsize):
+      p[i,j] = [i, j]
+  d = np.zeros((SOMsize, SOMsize, 2))
+  u = np.zeros((SOMsize, SOMsize, 2))
+  m = np.zeros((SOMsize, SOMsize, 3))
+  while error>tolerance and eta>0.1 and sigma>1:
+    iter = iter+1
+    Wold = np.copy(Wnew)
+    i = random.randint(0, N-1)
+    xtrain = X[i,:] / np.linalg.norm(X[i,:])
+    a = (np.matmul(xtrain, Wold)).reshape((SOMsize, SOMsize))
+    [row, col] = np.unravel_index(np.argmax(a), a.shape)
+    d = np.linalg.norm((p - np.asarray([row, col])), axis=2)
+    u = np.exp((-1) * (d**2) / (2*(sigma**2)))
+    Wnew = Wold + (eta*(u.flatten())) * (xtrain.reshape(3,1) - Wold)
+    Wnew = Wnew / np.linalg.norm(Wnew, axis=0)
+    eta = eta*alpha
+    sigma = sigma*alpha 
+    error = np.linalg.norm(Wnew[:,(SOMsize*(row)+col)] - Wold[:,(SOMsize*(row)+col)])
+    if saveAnimation:
+      imgs = animate_gradient(Wnew, n, m, imgs)
+  print("error: %f" % error)
+  print("eta: %f" % eta)
+  print("sigma: %f" % sigma)
+  for i in range(n):
+    m[:,:,i] = (Wnew[i,:]).reshape((SOMsize, SOMsize))
+  for i in range(N):
+    xtrain = X[i,:] / np.linalg.norm(X[i,:])
+    a = (np.matmul(xtrain, Wnew)).reshape((SOMsize, SOMsize))
+    [row, col] = np.unravel_index(np.argmax(a), a.shape)
+    winningNodes.add((row, col))
+  print('starting animation')
+  if saveAnimation:
+    imgs = animate_nodes(winningNodes, m, imgs)
+  print('that took ' + str(iter) + ' iterations')
+  return m, winningNodes, fig, imgs
+
+def get_som_input(N):
+  rows = N**3
+  X = np.zeros((rows, 3))
+  intensities = np.linspace(0,255,N)
+  for r in range(N):
+    for g in range(N):
+      for b in range(N):
+        X[(N**2)*r+N*g+b,:] = [intensities[r], intensities[g], intensities[b]]
+  filtered = []
+  for i in range(N**3):
+    if (X[i,0] != X[i,1] or X[i,0] != X[i,2] or X[i,1] != X[i,2]):
+      filtered.append(X[i])
+  filtered = np.asarray(filtered)
+  return filtered
+
+def plot_som(som, winningNodes):
+  winningPlot = np.zeros((som.shape))
+  print('len = ' + str(len(winningNodes)))
+  for (row, col) in winningNodes:
+    winningPlot[row,col] = som[row,col]
+  plot1 = plt.figure(1)
+  plt.imshow(winningPlot)
+
+  plot2 = plt.figure(2)
+  plt.imshow(som)
+  plt.show()
+
+start = time.time()
+
+ROWS = 30
+SOMsize = 100
+vidName = 'animated_som_100.mp4'
+saveAnimation = True
+
+input = get_som_input(ROWS)
+
+som, winningNodes, fig, imgs = gen_som(input, SOMsize, saveAnimation)
+
+end = time.time()
+print("runtime: %f" % ((end - start)/60))
+
+if saveAnimation:
+  somAnimation = animation.ArtistAnimation(fig, imgs)
+  #Writer = animation.writers['ffmpeg']
+  writer = animation.FFMpegWriter(fps=15, bitrate=50)
+  #writer = Writer(fps=15, bitrate=300)
+  print("saving animation...")
+  somAnimation.save(vidName, writer=writer)
+  print("saved")
+
+# plot_som(som, winningNodes)
+# print(som)
